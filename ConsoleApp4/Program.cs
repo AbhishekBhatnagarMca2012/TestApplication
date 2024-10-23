358748c864b326df63290a246eed287ed398c9e0
AbhishekBhatnagarMca2012 <abhishek.bhatnagar.mca.2012@gmail.com>
2024-10-23 08:54:47 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int[] vs = new int[] { 2,7,11, 15 }; // need to sort
+            vs = vs.OrderBy(i => i).ToArray();
+            int target = 37;
+
+            List<int> res = GetData(vs, target);
+
+            if (res != null)
+            {
+                foreach (var item in res)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+
+        }
+
+
+        public static List<int> GetData(int[] numbers, int target)
+        { 
+        
+            List<int> list = new List<int>();
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                list = new List<int>();
+                sum = 0;
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    list.Add(numbers[j]);
+                    sum += numbers[j];
+
+                    if (sum == target)
+                    {
+                        return list;
+                    }
+                    if (sum > target)
+                    {
+                        break;
+                    }
+
+                }
+            }
+           
+
+            return null;
+        }
+    }
+
+}
